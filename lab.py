65281cbf6faeb4f2c4f21bf99a6440d512b7654f
Sarah <sjbe150@cse.unsw.edu.au>
2013-09-18 12:39:57 +1000

    add -f functionality: show labs by how many computers they have free

--- lab.py
+++ lab.py
@@ -4,17 +4,42 @@ import subprocess, sys, re
 def get_names(x):
 	output = subprocess.check_output(["./getusername.sh", x.group(1)]).strip().split(" ")
 	nameString = ": %s %s (%s)" % (output[0], output[-1], x.group(1))
-	return nameString + " " * (32 - len(nameString))
+	return nameString + " " * (48 - len(nameString))
 
 
-data = subprocess.check_output(["lab", "-A"]).split("\n")
+def labstalk():
+	data = subprocess.check_output(["lab", "-A"]).split("\n")
 
-for line in data:
+	for line in data:
+		
+		if re.search(": \w", line):
+			print ": \t".join(re.sub(": (\w+)\s+", get_names, line).split(": "))
+		elif ("-q" not in sys.argv):
+			print " ".join(line.split(" ")[:-2])	
+
+
+def find_a_lab():
+	data = subprocess.check_output(["lab", "-A"]).split("\n")
+	for line in data:
+		if "Lab" in line:
+			if "FREE" in line:
+				splitline = line.split(" ")
+				free = -1 # because we match the lab line too
+				for labline in data:
+					if splitline[1] in labline:
+						if not re.search(": \w", labline):
+							free+=1
+					
+					
+				print "%s has %d free computers" % (splitline[1], free)								
+
 	
-	if re.search(": \w", line):
-		print ": \t".join(re.sub(": (\w+)\s+", get_names, line).split(": "))
-	elif ("-q" not in sys.argv):
-		print " ".join(line.split(" ")[:-2])	
-
 
 	
+if __name__ == '__main__':
+
+	if "-f" in sys.argv:
+		find_a_lab()
+	else:
+		labstalk()
+
